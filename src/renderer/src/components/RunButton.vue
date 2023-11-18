e55b0172f9ee3b305b45537712499d3c4902eaf9
Finestwork <Younes.Espiritu@outlook.com>
2023-11-18 20:12:02 +0800

    Add bouncy animation to run button

--- src/renderer/src/components/RunButton.vue
+++ src/renderer/src/components/RunButton.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import RunButtonIcon from '@components/RunButtonIcon.vue';
+import { bounceAnimation } from '@composables/useButtonAnimation';
+
+// NPM
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+
+const runBtn: Ref<HTMLButtonElement | null> = ref(null);
+
+const onClickRunCode = () => {
+  bounceAnimation(runBtn);
+  runBtn?.value?.blur?.();
+};
 </script>
 <template>
   <button
     class="group absolute right-2 top-0 z-10 flex items-center rounded-md bg-button-dark px-3 py-2 shadow-button-dark-float hover:bg-button-dark-hover hover:shadow-button-dark-float-hover focus:bg-button-dark-hover focus:shadow-button-dark-float-hover"
+    ref="runBtn"
     type="button"
+    @click="onClickRunCode"
   >
     <span class="mr-2 block w-[8px]">
       <RunButtonIcon class="fill-button-text-dark group-hover:fill-white group-focus:fill-white" />
