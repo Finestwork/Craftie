3499f3e864d40817febcb7512e39ed926a54e559
Finestwork <Younes.Espiritu@outlook.com>
2023-11-18 19:50:28 +0800

    Change color scheme of run button

--- src/renderer/src/components/RunButton.vue
+++ src/renderer/src/components/RunButton.vue
@@ -3,12 +3,15 @@ import RunButtonIcon from '@components/RunButtonIcon.vue';
 </script>
 <template>
   <button
-    class="absolute right-2 top-0 z-10 flex items-center rounded-md bg-button-dark px-3 py-2 shadow-button-dark-float hover:bg-button-dark-hover hover:shadow-button-dark-float-hover"
+    class="group absolute right-2 top-0 z-10 flex items-center rounded-md bg-button-dark px-3 py-2 shadow-button-dark-float hover:bg-button-dark-hover hover:shadow-button-dark-float-hover focus:bg-button-dark-hover focus:shadow-button-dark-float-hover"
     type="button"
   >
     <span class="mr-2 block w-[8px]">
-      <RunButtonIcon />
+      <RunButtonIcon class="fill-button-text-dark group-hover:fill-white group-focus:fill-white" />
     </span>
-    <span class="text-xs font-bold text-button-text-dark">Run</span>
+    <span
+      class="text-xs font-bold text-button-text-dark group-hover:text-white group-focus:text-white"
+      >Run</span
+    >
   </button>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,12 @@ module.exports = {
         ['editor-dark']: '#0F111A',
         ['title-bar-dark']: '#090B10',
         ['button-dark']: '#292B35',
-        ['button-dark-hover']: '#333743',
+        ['button-dark-hover']: '#40BAEB',
         ['button-text-dark']: '#858585'
       },
       boxShadow: {
-        ['button-dark-float']: 'inset 0 -2px 0 0 #303340',
-        ['button-dark-float-hover']: 'inset 0 -2px 0 0 #4F5464'
+        ['button-dark-float']: 'inset 0 -3px 0 0 #303340',
+        ['button-dark-float-hover']: 'inset 0 -3px 0 0 #124154'
       }
     }
   },
