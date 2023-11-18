bb4b89c99079cc707f24c185df6da789b6794fc6
Finestwork <Younes.Espiritu@outlook.com>
2023-11-18 17:18:39 +0800

    Add logo in title bar

--- src/renderer/src/components/TitleBar.vue
+++ src/renderer/src/components/TitleBar.vue
@@ -1,9 +1,13 @@
+<script setup lang="ts">
+import TitleBarLogo from '@components/TitleBarLogo.vue';
+</script>
+
 <template>
   <div
     class="drag-region fixed left-0 top-0 z-50 flex h-[32px] w-full select-none justify-between bg-title-bar-dark"
   >
     <div class="no-drag-region flex">
-      <button type="button">Close</button>
+      <TitleBarLogo />
     </div>
   </div>
 </template>
---
+++ src/renderer/src/components/TitleBarLogo.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Logo from '@components/Logo.vue';
+</script>
+
+<template>
+  <span class="block w-[50px] p-2">
+    <Logo />
+  </span>
+</template>
