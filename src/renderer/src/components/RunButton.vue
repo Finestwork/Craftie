6591c5b6820964e5e98055509bf0b208bc8c347c
Finestwork <Younes.Espiritu@outlook.com>
2023-11-18 19:29:53 +0800

    Fix run button position

--- src/renderer/src/components/RunButton.vue
+++ src/renderer/src/components/RunButton.vue
@@ -3,12 +3,12 @@ import RunButtonIcon from '@components/RunButtonIcon.vue';
 </script>
 <template>
   <button
-    class="absolute top-[30px] right-2 z-10 bg-button-dark flex items-center px-3 py-2 rounded-md shadow-button-dark-float hover:bg-button-dark-hover hover:shadow-button-dark-float-hover"
+    class="absolute right-2 top-0 z-10 flex items-center rounded-md bg-button-dark px-3 py-2 shadow-button-dark-float hover:bg-button-dark-hover hover:shadow-button-dark-float-hover"
     type="button"
   >
-    <span class="w-[8px] block mr-2">
+    <span class="mr-2 block w-[8px]">
       <RunButtonIcon />
     </span>
-    <span class="font-bold text-button-text-dark text-xs">Run</span>
+    <span class="text-xs font-bold text-button-text-dark">Run</span>
   </button>
 </template>
