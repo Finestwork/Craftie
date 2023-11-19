753599e353148ed58792bec6fe48a34acde50909
Finestwork <Younes.Espiritu@outlook.com>
2023-11-19 23:40:00 +0800

    Add empty state message

--- src/renderer/src/components/TheEmptyState.vue
+++ src/renderer/src/components/TheEmptyState.vue
@@ -5,12 +5,22 @@ import SassIcon from '@components/SassIcon.vue';
 </script>
 
 <template>
-    <div class="flex h-full items-center justify-center bg-editor-dark">
+    <div class="relative flex h-full items-center justify-center bg-editor-dark">
         <BaseEmptyStateButton>
             <JavaScriptIcon />
         </BaseEmptyStateButton>
         <BaseEmptyStateButton>
             <SassIcon />
         </BaseEmptyStateButton>
+
+        <p
+            class="absolute bottom-4 left-0 w-full text-center text-sm font-medium text-tab-foreground"
+        >
+            Let's get that file created, and let the
+            <span class="border-b-2 border-dashed border-[#9bddf7] text-[#9bddf7]"
+                >coding adventure</span
+            >
+            begin!
+        </p>
     </div>
 </template>
