2e7084158ab72ae199f38af57233f1a8cf46240e
Finestwork <Younes.Espiritu@outlook.com>
2023-11-20 22:46:25 +0800

    Change add button color schemes

--- src/renderer/src/components/AddFileButtonIcon.vue
+++ src/renderer/src/components/AddFileButtonIcon.vue
@@ -3,11 +3,11 @@ import PlusIcon from '@components/PlusIcon.vue';
 </script>
 <template>
     <button
-        class="group block h-[36px] w-[38px] bg-[#292e3f] p-[12px] hover:bg-tab-hover focus:bg-tab-hover"
+        class="group block h-[36px] w-[38px] bg-[#1a1e2b] p-[12px] hover:bg-tab-hover focus:bg-tab-hover"
         type="button"
     >
         <PlusIcon
-            class="stroke-[#5f6684] group-hover:stroke-tab-foreground-hover group-focus:stroke-tab-foreground-hover"
+            class="stroke-[#3d4768] group-hover:stroke-tab-foreground-hover group-focus:stroke-tab-foreground-hover"
         />
     </button>
 </template>
