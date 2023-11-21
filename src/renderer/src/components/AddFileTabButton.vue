f0362289234fa4d2ea523c479472616dcafc2b8f
Finestwork <Younes.Espiritu@outlook.com>
2023-11-21 14:08:00 +0800

    Add functionality to navigate tab button dropdown component with up/down keyboard keys

--- src/renderer/src/components/AddFileTabButton.vue
+++ src/renderer/src/components/AddFileTabButton.vue
@@ -13,6 +13,35 @@ const tabButtonDropdown = ref();
 const showDropdown = ref(false);
 const { activate, deactivate } = useFocusTrap(tabButtonDropdown);
 
+const handleFocusElement = (e) => {
+    // If dropdown is not shown up, do nothing
+    if (!showDropdown.value) return;
+
+    const CurrentElement = <HTMLElement>document.activeElement;
+    if (e.key.toLowerCase() === 'arrowdown') {
+        const NextElement = <HTMLElement>CurrentElement.nextElementSibling;
+        if (!NextElement) {
+            const ParentElement = <HTMLElement>CurrentElement.parentElement;
+            const FirstChild = <HTMLElement>ParentElement.firstElementChild;
+            if (!FirstChild) return;
+            FirstChild.focus();
+            return;
+        }
+        NextElement.focus();
+    }
+    if (e.key.toLowerCase() === 'arrowup') {
+        const PreviousElement = <HTMLElement>CurrentElement.previousElementSibling;
+        if (!PreviousElement) {
+            const ParentElement = <HTMLElement>CurrentElement.parentElement;
+            const FirstChild = <HTMLElement>ParentElement.lastElementChild;
+            if (!FirstChild) return;
+            FirstChild.focus();
+            return;
+        }
+        PreviousElement.focus();
+    }
+};
+
 onMounted(() => {
     window.electron.ipcRenderer.on('onShortcutOpenTabDropdown', () => {
         showDropdown.value = true;
@@ -20,6 +49,7 @@ onMounted(() => {
     window.electron.ipcRenderer.on('onShortcutCloseTabDropdown', () => {
         showDropdown.value = false;
     });
+    document.body.addEventListener('keydown', handleFocusElement);
 });
 
 onClickOutside(target, () => {
