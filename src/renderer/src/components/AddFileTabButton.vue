78de1774b9bb7d41c680b3e147b320f79056136a
Finestwork <Younes.Espiritu@outlook.com>
2023-11-21 14:42:16 +0800

    Make dropdown navigation function as composable

--- src/renderer/src/components/AddFileTabButton.vue
+++ src/renderer/src/components/AddFileTabButton.vue
@@ -3,6 +3,8 @@ import BaseButtonDropdown from '@components/BaseButtonDropdown.vue';
 import AddFileButtonIcon from '@components/AddFileButtonIcon.vue';
 import TabButtonDropdown from '@components/TabButtonDropdown.vue';
 
+import { useTabDropdownNavigation } from '@composables/useTabDropdownNavigation';
+
 // NPM
 import { nextTick, onMounted, ref, watch } from 'vue';
 import { onClickOutside } from '@vueuse/core';
@@ -12,35 +14,8 @@ const target = ref();
 const tabButtonDropdown = ref();
 const showDropdown = ref(false);
 const { activate, deactivate } = useFocusTrap(tabButtonDropdown);
-
-const handleFocusElement = (e) => {
-    // If dropdown is not shown up, do nothing
-    if (!showDropdown.value) return;
-
-    const CurrentElement = <HTMLElement>document.activeElement;
-    if (e.key.toLowerCase() === 'arrowdown') {
-        const NextElement = <HTMLElement>CurrentElement.nextElementSibling;
-        if (!NextElement) {
-            const ParentElement = <HTMLElement>CurrentElement.parentElement;
-            const FirstChild = <HTMLElement>ParentElement.firstElementChild;
-            if (!FirstChild) return;
-            FirstChild.focus();
-            return;
-        }
-        NextElement.focus();
-    }
-    if (e.key.toLowerCase() === 'arrowup') {
-        const PreviousElement = <HTMLElement>CurrentElement.previousElementSibling;
-        if (!PreviousElement) {
-            const ParentElement = <HTMLElement>CurrentElement.parentElement;
-            const FirstChild = <HTMLElement>ParentElement.lastElementChild;
-            if (!FirstChild) return;
-            FirstChild.focus();
-            return;
-        }
-        PreviousElement.focus();
-    }
-};
+const { activate: dropdownNavigationActivate, deactivate: dropdownNavigationDeactivate } =
+    useTabDropdownNavigation(tabButtonDropdown);
 
 onMounted(() => {
     window.electron.ipcRenderer.on('onShortcutOpenTabDropdown', () => {
@@ -49,7 +24,6 @@ onMounted(() => {
     window.electron.ipcRenderer.on('onShortcutCloseTabDropdown', () => {
         showDropdown.value = false;
     });
-    document.body.addEventListener('keydown', handleFocusElement);
 });
 
 onClickOutside(target, () => {
@@ -59,8 +33,13 @@ watch(
     () => showDropdown.value,
     async (isActive) => {
         await nextTick();
-        if (isActive) activate();
-        else deactivate();
+        if (isActive) {
+            activate();
+            dropdownNavigationActivate();
+        } else {
+            deactivate();
+            dropdownNavigationDeactivate();
+        }
     },
     { flush: 'post' }
 );
---
+++ src/renderer/src/composables/useTabDropdownNavigation.ts
@@ -0,0 +1,45 @@
+import { unref, ref, Ref, onMounted, onUnmounted, nextTick } from 'vue';
+
+export const useTabDropdownNavigation = (wrapper: Ref<HTMLElement>) => {
+    let buttons: Ref<NodeListOf<HTMLButtonElement>> | Ref<null> = ref(null);
+    let currentActiveIndex = -1;
+
+    const handleNavigation = (e: KeyboardEvent) => {
+        if (buttons.value === null) return;
+
+        const CurrentArrayLength = buttons.value.length - 1;
+        if (e.key.toLowerCase() === 'arrowdown') {
+            if (currentActiveIndex === CurrentArrayLength) {
+                currentActiveIndex = 0;
+            } else {
+                currentActiveIndex++;
+            }
+        }
+
+        if (e.key.toLowerCase() === 'arrowup') {
+            if (currentActiveIndex === 0) {
+                currentActiveIndex = CurrentArrayLength;
+            } else {
+                currentActiveIndex--;
+            }
+        }
+
+        buttons.value[currentActiveIndex].focus();
+    };
+
+    const activate = () => {
+        const Wrapper = unref(wrapper);
+        buttons.value = Wrapper.querySelectorAll('button');
+        currentActiveIndex = Array.from(buttons.value).findIndex(
+            (button) => button === document.activeElement
+        );
+        currentActiveIndex = currentActiveIndex !== -1 ? currentActiveIndex : 0;
+
+        document.body.addEventListener('keydown', handleNavigation);
+    };
+    const deactivate = () => {
+        document.body.removeEventListener('keydown', handleNavigation);
+    };
+
+    return { activate, deactivate };
+};
