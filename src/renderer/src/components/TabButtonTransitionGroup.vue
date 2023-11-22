e8696b661b085aba65befb9837b8622cfcb37ccc
Finestwork <Younes.Espiritu@outlook.com>
2023-11-22 23:20:45 +0800

    Make transition as composable

--- src/renderer/src/components/TabButtonTransitionGroup.vue
+++ src/renderer/src/components/TabButtonTransitionGroup.vue
@@ -1,58 +1,7 @@
 <script setup lang="ts">
-import anime from 'animejs/lib/anime';
+import { useTabTransition } from '@composables/useTabTransition';
 
-const onBeforeEnter = (el) => {
-    Object.assign(el.style, {
-        width: 0
-    });
-    Object.assign(el.firstElementChild.style, {
-        transform: 'translateY(26px)'
-    });
-};
-const onEnter = (el, done) => {
-    const FirstElementChild = el.firstElementChild;
-    const Timeline = anime.timeline({
-        easing: 'easeOutCubic',
-        duration: 350,
-        complete: () => {
-            el.style = null;
-            FirstElementChild.style = null;
-            done();
-        }
-    });
-
-    Timeline.add({
-        targets: el,
-        width: 150
-    });
-
-    Timeline.add({
-        targets: FirstElementChild,
-        translateY: 0
-    });
-};
-
-const onLeave = (el, done) => {
-    const FirstElementChild = el.firstElementChild;
-    const Timeline = anime.timeline({
-        easing: 'easeOutCubic',
-        complete: () => {
-            done();
-        }
-    });
-
-    Timeline.add({
-        targets: FirstElementChild,
-        scale: 0,
-        duration: 150
-    });
-
-    Timeline.add({
-        targets: el,
-        width: 0,
-        duration: 350
-    });
-};
+const { onBeforeEnter, onEnter, onLeave } = useTabTransition();
 </script>
 <template>
     <TransitionGroup
