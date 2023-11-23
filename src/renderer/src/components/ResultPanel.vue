bb660f32521cc73989d142ec5d3a105a8945a6a6
Finestwork <Younes.Espiritu@outlook.com>
2023-11-24 05:09:51 +0800

    Change result panel component UI

--- src/renderer/src/components/ResultPanel.vue
+++ src/renderer/src/components/ResultPanel.vue
@@ -1,22 +1,21 @@
 <script setup lang="ts">
-import ScssResult from '@components/ScssResult.vue';
 import { displayResult } from '@composables/ipcListeners/useCodeHandler';
+import { computed } from 'vue';
 
 const { result, isError } = displayResult();
+const Code = computed(() => {
+    if (Array.isArray(result.value)) {
+        return result.value.join('\n');
+    }
+    return result.value;
+});
 </script>
 
 <template>
-    <div class="scrollbar h-full bg-editor-dark pl-[10px] font-jb text-sm font-semibold text-white">
-        <div v-if="Array.isArray(result)" class="h-full overflow-y-scroll pb-4">
-            <p v-for="(code, ind) in result" :key="`${code}${ind}`" class="mb-2 last-of-type:mb-0">
-                {{ code }}
-            </p>
-        </div>
-        <template v-else>
-            <ScssResult v-if="!isError" :code="result" />
-            <pre v-else class="text-red-400">
-                {{ result }}
-            </pre>
-        </template>
+    <div
+        class="h-full overflow-y-scroll bg-editor-dark pl-[10px] font-jb text-sm font-semibold"
+        :class="{ 'text-red-400': isError, 'text-white': !isError }"
+    >
+        <pre>{{ Code }}</pre>
     </div>
 </template>
